b48c0ce879d359eedb6874cced6874479c61f01f
RTsaban <Ronen.Tsaban@verint.com>
2016-07-10 00:27:29 +0300

    Solution for: sol-python-verint.syntax-lab/01.py

--- 05_syntax_lab/01.py
+++ 05_syntax_lab/01.py
@@ -1,5 +1,11 @@
-"""
-Write a program that reads 10 numbers from
-the user and prints the largest one
-"""
+print "Enter Number"
+big = int(raw_input())
 
+for i in range(1,10) :
+    print "Enter Number"
+    num = int(raw_input())
+    big = max(num,big)
+
+print "Max Number is %d",big
+
+    
